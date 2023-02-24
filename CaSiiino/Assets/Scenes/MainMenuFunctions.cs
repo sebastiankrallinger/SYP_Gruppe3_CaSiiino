dc86730ed2ec67cad3a982d1c56073ec36297685
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2023-02-24 10:31:43 +0100

    Add Functions Mainmenu
    Scene Managment

---
+++ CaSiiino/Assets/Scenes/MainMenuFunctions.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Go to Roulette Scene
+    public void goToRouletteScene()
+    {
+        SceneManager.LoadScene("RouletteScene");
+    }
+    //Go to UNO Scene
+    public void goToUnoScene()
+    {
+        SceneManager.LoadScene("UnoScene");
+    }
+    //Go to Horserace Scene
+    public void goToHorseraceScene()
+    {
+        SceneManager.LoadScene("HorseraceScene");
+    }
+    //Go to 11m Scene
+    public void goTo11mScene()
+    {
+        SceneManager.LoadScene("11mScene");
+    }
+    //Go to Settings Scene
+    public void goToSettingsScene()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+    //Exit Game
+    public void extiGame()
+    {
+        Application.Quit();
+    }
+}
