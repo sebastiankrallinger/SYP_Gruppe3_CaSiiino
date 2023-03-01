c60caa83daf7546fa70e7de1e82bc4dfa0ddc95a
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2023-03-01 17:38:15 +0100

    Intro fix

--- CaSiiino/Assets/Scenes/IntroFunction.cs
+++ CaSiiino/Assets/Scenes/IntroFunction.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class IntroFunction : MonoBehaviour
 {
-    float wait_time = 5f;
+    public float wait_time = 0f;
     // Start is called before the first frame update
     void Start()
     {
--- CaSiiino/Assets/Scenes/MainMenuFunctions.cs
+++ CaSiiino/Assets/Scenes/MainMenuFunctions.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenuFunctions : MonoBehaviour
+public class MainmenuFunctions : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,6 @@ public class MainMenuFunctions : MonoBehaviour
     {
         
     }
-
     //Go to Roulette Scene
     public void goToRouletteScene()
     {
